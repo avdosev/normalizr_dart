8d1e3489ab1a03f18b4141532269c0e57ae1f7e6
Nikita Avdosev <avdosev1@yandex.ru>
2022-02-21 21:50:21 +0300

    init

---
+++ lib/normalizr.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library normalizr;
+
+export 'src/normalizr_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
